85f0e1377755979958a000f42eed9cf4ab8a61aa
Asa Smith <smith.asa.la@gmail.com>
2018-04-10 19:49:06 -0400

    change exports to export

--- helpers/build-embed.js
+++ helpers/build-embed.js
@@ -1,4 +1,4 @@
-module exports = function (_, videoId) {
+module export = function (_, videoId) {
   return '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://player.vimeo.com/video/' + videoId + '"' + ' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
 }
 
