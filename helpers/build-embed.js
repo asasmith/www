317b345f676446e1a59e1f1e466bedd979cc91e5
Asa Smith <smith.asa.la@gmail.com>
2018-04-10 19:51:31 -0400

    module.exports, actually

--- helpers/build-embed.js
+++ helpers/build-embed.js
@@ -1,4 +1,4 @@
-module export = function (_, videoId) {
+module.exports = function (_, videoId) {
   return '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://player.vimeo.com/video/' + videoId + '"' + ' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
 }
 
